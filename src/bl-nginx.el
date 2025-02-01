ffeefd1ae21be14f87aae3f3845d01e35f4e13f3
Piotr Klibert <piotrklibert@fastmail.com>
2025-02-01 01:11:14 +0100

    Static files handling with rsync made async, working commands for docker

--- src/bl-nginx.el
+++ src/bl-nginx.el
@@ -1,11 +1,12 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 
-(defconst bl-docker-image "openresty/openresty:alpine-fat")
+(defconst bl-docker-image "openresty/openresty:latest")
 (defconst bl-docker-container "blog-nginx")
 (defconst bl-docker-port 32769)
 (defconst bl-docker-mount "/site")
 (defconst bl-docker-conf "/etc/nginx/conf.d/default.conf")
 (defconst bl-nginx-conf "/home/cji/priv/blog/server/server.conf")
+
 
 (cl-defun bl-docker-mount-format (from to &optional (ro t))
   (unless (f-absolute-p from)
@@ -14,14 +15,24 @@
 
 
 (defconst bl-docker-run-cmd
-  (list
-   "docker" "run" "--rm" "--name" bl-docker-container
-   "-v" (bl-docker-mount-format bl-nginx-conf bl-docker-conf)
-   "-v" (bl-docker-mount-format "build" bl-docker-mount)
-   "-p" "32769:80"
-   bl-docker-image))
-;; (s-join " " bl-docker-run-cmd)
-;; docker run --rm --name blog-nginx -v /home/cji/priv/blog/server/server.conf:/etc/nginx/conf.d/default.conf:ro -v /home/cji/priv/blog/build:/site:ro -p 32769:80 openresty/openresty:alpine-fat
+  (list "docker" "run" "--rm" "--detach"
+        "--name" bl-docker-container
+        "-v" (bl-docker-mount-format bl-nginx-conf bl-docker-conf)
+        "-v" (bl-docker-mount-format "build" bl-docker-mount nil)
+        "-p" "80:80"
+        bl-docker-image))
 
+;; (bl-start-serving-files)
+(cl-defun bl-start-serving-files ()
+  (interactive)
+  (start-process-shell-command "run-docker" "*run-docker*"
+    (s-join " " bl-docker-run-cmd))
+  (message "Serving files at http://localhost:80 - use M-x docker to stop it"))
+
+;; (bl-stop-serving-files)
+(cl-defun bl-stop-serving-files ()
+  (interactive)
+  (start-process-shell-command "stop-docker" "*stop-docker*"
+    (format "docker stop %s" bl-docker-container)))
 
 (provide 'bl-nginx)
--- src/bl-statics.el
+++ src/bl-statics.el
@@ -1,17 +1,47 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
-(require 'my-threading)
+
+(require 'async-await)
+(require 'promise)
+
+(cl-defun bl-async-rsync (cmd)
+  (let ((default-directory bl-blog-root))
+    (promise-then
+        (promise:make-process-with-handler (list "bash" "-c" cmd) nil t)
+      (lambda (result)
+        (promise-resolve (list :success t :message result :reason nil :cmd cmd)))
+      (lambda (reason)
+        (promise-resolve (list :success nil
+                               :cmd cmd
+                               :reason (s-trim (car reason))
+                               :message (cadr reason)))))))
+
+
+(async-defun bl-copy-data-files-1 ()
+  (let ((res (await (promise-all
+                     (list (bl-async-rsync "rsync -av posts/*.el build/posts/")
+                           (bl-async-rsync "rsync --dry-run -crav posts/data build/posts/")
+                           (bl-async-rsync "rsync --dry-run -crav statics build/"))))))
+    (let ((fails (cl-loop for r across res when (not (plist-get r :success))
+                          collect r)))
+      (if fails
+          (promise-reject fails)
+        (promise-resolve t)))))
+
+(cl-defun bl-copy-data--format-error (fail)
+  (let ((msg (->> (plist-get fail :message) (s-split "\n") (--map (s-concat "    " it)) (s-join "\n"))))
+    (message "  %s: %s\n\n%s\n    ======\n"
+             (plist-get fail :cmd)
+             (plist-get fail :reason)
+             msg)))
 
 (cl-defun bl-copy-data-files ()
   (interactive)
-  (mt-thread
-   (lambda ()
-     (mt-message
-      (concat
-       "Handling data files...\n"
-       (mt-shell "rsync -cav posts/*.el build/posts/")
-       (mt-shell "rsync -crav posts/data build/posts/")
-       (mt-shell "rsync -crav statics build/")
-       "Handling data files...done\n")))))
+  (message "Handling data files...\n")
+  (promise-then (bl-copy-data-files-1)
+    (lambda (_) (message "All files transferred."))
+    (lambda (fails)
+      (message "Failed to copy some data files:\n")
+      (cl-loop for f in fails do (bl-copy-data--format-error f)))))
 
 
 (provide 'bl-statics)
